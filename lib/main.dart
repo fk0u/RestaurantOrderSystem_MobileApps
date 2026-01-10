16216a39e3c22b59991ecf609b8f65a3aed4fefb
Al-Ghani Desta Setyawan <kou.real@outlook.com>
2026-01-10 11:27:08 +0800

    feat: improve

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'core/theme/app_theme.dart';
 import 'core/router/app_router.dart';
 import 'core/services/storage_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi;
+  }
   
   final storageService = await StorageService.init();
 
