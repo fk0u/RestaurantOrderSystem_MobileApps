4d84361e1930297971399f523d57ee0d79b68b82
Al-Ghani Desta Setyawan <kou.real@outlook.com>
2026-01-25 14:11:37 +0800

    refactor(database): remove SQLite helper and dependencies for MySQL integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'core/theme/app_theme.dart';
 import 'core/router/app_router.dart';
 import 'core/services/storage_service.dart';
@@ -10,11 +8,6 @@ import 'core/services/pusher_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
-  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
-    sqfliteFfiInit();
-    databaseFactory = databaseFactoryFfi;
-  }
   
   final storageService = await StorageService.init();
   await NotificationService.init();
